92a2616b482ed51917690c84c4cb12f5c20d15cb
Riley Mathews <rileymathews80@gmail.com>
2018-04-24 12:22:49 -0500

    finished exercise

--- powers.js
+++ powers.js
@@ -6,16 +6,46 @@
         2. Add the `enabled` class
 
 */
-document.querySelector("#activate-flight").addEventListener(event, handlerFunction)
+document.querySelector("#activate-flight").addEventListener("click", () => {
+    const power = document.querySelector("#flight")
+    power.classList.remove("disabled")
+    power.classList.add("enabled")
+})
 
 /*
     Now write two more event handlers that activate the other two powers
     when the corresponding buttons are clicked.
 */
+document.querySelector("#activate-mindreading").addEventListener("click",() => {
+    const power = document.querySelector("#mindreading")
+    power.classList.remove("disabled")
+    power.classList.add("enabled")
+})
 
+document.querySelector("#activate-xray").addEventListener("click",() => {
+    const power = document.querySelector("#xray")
+    power.classList.remove("disabled")
+    power.classList.add("enabled")
+})
 
 /*
     Write two more event handlers for activating and deactivating all powers
     when the corresponding buttons are clicked. You will need to use the
     `document.querySelectorAll()` method for these.
 */
+
+document.querySelector("#activate-all").addEventListener("click",() => {
+    const powers = document.querySelectorAll(".power")
+    powers.forEach(power => {
+        power.classList.remove("disabled")
+        power.classList.add("enabled")
+    })
+})
+
+document.querySelector("#deactivate-all").addEventListener("click",() => {
+    const powers = document.querySelectorAll(".power")
+    powers.forEach(power => {
+        power.classList.remove("enabled")
+        power.classList.add("disabled")
+    })
+})
